fa48bbb9181c6a5c72eaf0b031fdd70ab4a1d9cc
Bobbins228 <markcampbell114@gmail.com>
2022-02-08 20:08:27 +0000

    Added JSON Storage

--- app/src/main/java/com/mymovielist/models/MovieListStore.kt
+++ app/src/main/java/com/mymovielist/models/MovieListStore.kt
@@ -2,6 +2,8 @@ package com.mymovielist.models
 
 interface MovieListStore {
     fun findAll() : List<MovieListModel>
-    fun findById(id: Long) : MovieListModel?
+    //fun findById(id: Long) : MovieListModel?
     fun create(movie: MovieListModel)
+    fun update(movie: MovieListModel)
+    fun delete(movie: MovieListModel)
 }
